d7acb7a805c15ba080ad304e38b588507d72ed98
Biii88 <binht7949@gmail.com>
2024-08-27 21:14:20 +0700

    BaiTap

---
+++ src/homework3/Homework3.java
@@ -0,0 +1,21 @@
+package homework3;
+import java.util.Random;
+import java.util.Scanner;
+public class Homework3{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String check;
+        do {
+            int n = random.nextInt(100) + 1; 
+            System.out.println("Nhap vao check (Yes de tiep tuc): ");
+            check = scanner.nextLine();
+            if (n % 2 == 0){
+                System.out.println(n+" la so chan");
+            }else{
+                System.out.println(n+" la so le");
+            }
+        }while(check.equalsIgnoreCase("Yes"));
+        System.out.println("Ket thuc chuong trinh");
+    }
+}
